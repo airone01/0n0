096ebb9a524d144358d90eedb9645fa6dbce6da7
AirOne01 <21955960+AirOne01@users.noreply.github.com>
2024-02-06 11:23:11 +0100

    feat(theming): add theme toggle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import "@/styles/globals.css";
 import { Poppins } from "next/font/google";
 import { Konkhmer_Sleokchher } from "next/font/google";
 
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "@/components/ui/mode-toggle";
+
 const poppins = Poppins({
   weight: '500',
   subsets: ['latin'],
@@ -26,7 +29,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${poppins.className} ${konk.className} min-h-screen antialiased flex flex-col justify-center items-center`}>{children}</body>
+      <body className={`${poppins.className} ${konk.className} flex flex-col min-h-screen antialiased`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+        >
+          <nav className="grow-0 flex flex-row-reverse p-2">
+            <ModeToggle />
+          </nav>
+          <div className="h-full grow flex flex-col justify-center items-center">
+            {children}
+          </div>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
