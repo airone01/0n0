ec0bc582201a6d7a752c9db940c7ad543039d8b7
Erwann Lagouche <21955960+AirOne01@users.noreply.github.com>
2024-04-03 16:42:35 +0200

    refactor(ui): add toast

---
+++ packages/ui/src/components/toast/index.ts
@@ -0,0 +1,5 @@
+export {
+	Toast, ToastAction, type ToastActionElement, ToastClose, ToastDescription, type ToastProps, ToastProvider, ToastTitle, ToastViewport,
+} from '@/toast/toast';
+export {Toaster} from '@/toast/toaster';
+export {useToast} from '@/toast/use-toast';
